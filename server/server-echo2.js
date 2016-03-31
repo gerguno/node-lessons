fec4425ce8d8fd5bfa0cd2f1ebf6d66167378091
Oles Gergun <gerguno@gmail.com>
2016-03-31 17:40:56 +0300

    done 18 lesson

--- server/server-echo2.js
+++ server/server-echo2.js
@@ -7,15 +7,18 @@ var server = new http.Server(function(req,res) {
 	console.log(req.headers);
 	
 	var urlParsed = url.parse(req.url, true); // true важливе, бо розбирає строку 'message=Hello' в обєкт
+	debugger;
 
 	if (urlParsed.pathname == "/echo" && urlParsed.query.message) {
 		// res.writeHead(200, "OK", {'Cache-control': 'no-cache'});
 		res.setHeader('Cache-control', 'no-cache'); // шоб не кешувались response
 		res.end(urlParsed.query.message);
+		return;
 	} else {
 		res.statusCode = 404;
-		res.end('Page not found');
+		res.end('404: Page not found');
 	}		
 }); 
 
 server.listen(1337, '127.0.0.1');
+console.log('Server is running');
